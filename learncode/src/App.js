290cf2eddc8570d22bfeb289b29d1faf81defc22
AnimatedFred <fredrik.rosengren86@gmail.com>
2022-01-23 10:45:40 +0100

    add

--- learncode/src/App.js
+++ learncode/src/App.js
@@ -7,78 +7,32 @@ import styled from "styled-components";
 
 export const pageWrapper = styled.div`
 display: flex;
-background-color: #000;
-flex: 1;
-flex-direction: column;
-justify-content: stretched;
-align-items: stretched;
-width: 100%;
-@media (min-width: 767px) {
-  flex-direction: row;
-}
+
+width: 1080px;
+border: 2px dotted rgb(96, 139, 168);
+padding: 24px;
 `;
 
-export const ResponsiveColumn = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  justify-content: stretched;
-  align-items: stretched;
-  width: 100%;
-  @media (min-width: 767px) {
-    flex-direction: row;
-  }
-`;
-
-export const StyledProfile = styled.img`
-  
-  background-color: var(--accent);
-  border-radius: 16px;
-  width: 100px;
-  @media (min-width: 900px) {
-    width: 200px;
-  }
-  @media (min-width: 1000px) {
-    width: 200px;
-  }
-  transition: width 0.5s;
-`;
 
 
 
 function App() {
   return (
-    <pageWrapper>
-<ResponsiveColumn flex={1} style={{ padding: 24 }} test>
-      <s.Container   flex={1.5}
-            style={{
-              backgroundColor: "#fff",
-              borderColor:"#000",
-              padding: 24,
-              borderRadius: 0,
-            }}>
+    <screen>
+    <pageWrapper >
+<s.Container style={{
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'green',
+  }}>sssxkmsxmxkxkxs
   
-
-</s.Container>
-<s.Container   flex={4}
-          
-          style={{
-            backgroundColor: "#0B0B0C",
-            padding: 24,
-            borderRadius: 0,
-          }}>
-sssxkmsxmxkxkxs
-
-</s.Container>
+  </s.Container>
 
 
 
-          
-          </ResponsiveColumn>
-
-
-      
+    
 </pageWrapper>
+</screen>
       
       
   
